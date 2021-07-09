b1decebccbc2f6e72596967a9c6d06f4e58c23a4
MoustafaRateb <mostafa.rateb@cegedim.com>
2021-07-09 21:26:57 +0200

    Add new message

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace HelloCore
         static void Main(string[] args)
         {
             Console.WriteLine("Hello From Core!");
+            Console.WriteLine("Test auto build");
         }
     }
 }
